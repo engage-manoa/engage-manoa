3587e7f8c716dc24af577013d071ad039cff1fa4
Jun Ho Im <junhoim@hawaii.edu>
2020-11-13 11:43:59 -1000

    Landing page/ Theme
    Changed the website theme to green and updated landing page

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -6,34 +6,24 @@ class Landing extends React.Component {
   render() {
     return (
         <div className='backgroundImage'>
-          <Grid stackable centered container columns={3}>
-
-            <Grid.Column textAlign='center'>
+          <Grid stackable centered container columns={2}>
+            <Grid.Column textAlign='center'style={{marginTop: 200, marginBottom: 200}}>
               <Icon inverted size='huge' name='users'/>
               <Header inverted as='h1'>
-              Multiple Users
+              Join UHM Clubs
               </Header>
               <Header inverted as='h3'>
-              This address book enables any number of users to register and save their business contacts. You can only see the contacts you have created.
+              Using the list clubs feature you will be able to view all available clubs that is registered with engage-manoa
               </Header>
             </Grid.Column>
 
-            <Grid.Column textAlign='center'>
+            <Grid.Column textAlign='center' style={{marginTop: 200, marginBottom: 200}}>
               <Icon inverted size='huge' name='file alternate'/>
               <Header inverted as='h1'>
-                Contact Details
+                Create New Clubs
               </Header>
               <Header inverted as='h3'>
-                For each contact, you can save their name, address and phone number.
-              </Header>
-            </Grid.Column>
-            <Grid.Column textAlign='center'>
-              <Icon inverted size='huge' name='calendar check' />
-              <Header inverted as='h1'>
-                Timestamped Notes
-              </Header>
-              <Header inverted as='h3'>
-                Each time you make contact with a contact, you can write a note that summarizes the conversation. This note is saved along with a timestamp with the contact.
+                You can create your own club for other people to join!
               </Header>
             </Grid.Column>
         </Grid>
