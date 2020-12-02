6984aa154c76e8e7e5e0429c7885f5ce2be85c9a
mikel <mikel123@hawaii.rr.com>
2020-12-01 14:43:53 -1000

    Update Clubs.js

--- app/imports/api/club/Clubs.js
+++ app/imports/api/club/Clubs.js
@@ -26,10 +26,13 @@ class ClubsCollection {
       },
       Admin: String,
       category: {
-        type: String,
-        allowedValues: ['Academic/Professional', 'Religious/Spiritual', 'Leisure/Recreational', 'Honorary Society', 'Ethnic/Cultural', 'Service', 'Business', 'Engineering', 'Arts', 'Music', 'Other'],
+        type: Array,
         required: false,
         defaultValue: 'Other',
+      },
+      'category.$': {
+        type: String,
+        allowedValues: ['Academic/Professional', 'Religious/Spiritual', 'Leisure/Recreational', 'Honorary Society', 'Ethnic/Cultural', 'Service', 'Business', 'Engineering', 'Arts', 'Music', 'Other'],
       },
     }, { tracker: Tracker });
     // Attach the schema to the collection, so all attempts to insert a document are checked against schema.
