a5dc942d1e2731f87c65e54bcbea5256a1bfb67c
Christian Pak <cpak2019@users.noreply.github.com>
2020-12-01 22:45:25 -1000

    Fixed search functionality which broke when club collection was modified

--- app/imports/ui/pages/ListClubs.jsx
+++ app/imports/ui/pages/ListClubs.jsx
@@ -38,6 +38,7 @@ class ListClubs extends React.Component {
     this.setState({
       tagFilterState: tagFilter,
     });
+    console.log(tagFilter);
   }
 
   render() {
@@ -73,7 +74,7 @@ class ListClubs extends React.Component {
                         return true;
                       }
                       for (let i = 0; i < tagFilter.length; i++) {
-                         if (club.category === tagFilter[i]) {
+                         if (club.category.indexOf(tagFilter[i]) !== -1) {
                            return true;
                          }
                       }
