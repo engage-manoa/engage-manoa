8cf76006e11c6a884b687623e48bdcf234a2a8b0
Jun Ho Im <junhoim@hawaii.edu>
2020-12-02 10:19:14 -1000

    Extended the wait time for testController

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -14,7 +14,7 @@ fixture('engage-manoa localhost test with default db')
     .page('http://localhost:3000');
 
 test('Test that landing page shows up', async (testController) => {
-  testController.wait(500);
+  testController.wait(3000);
   await landingPage.isDisplayed(testController);
 });
 
